2e3e7584b06bfb42e548b076b850ea5a3ec1f9a7
Hirochon <heacet43@gmail.com>
2020-08-05 23:58:02 +0900

    [add] 2-4をクリア

---
+++ 02-javascript/04-chon.js
@@ -0,0 +1,31 @@
+class Bird {
+  constructor(name){
+    this.name = name;
+  }
+
+  chirp(){
+    console.log(`${this.name}が鳴きました`);
+  }
+
+  // クラスメソッドとして、インスタンスを生成しなくとも、いきなり使える！
+  static explain(name){
+    console.log(`${name}は翼があって卵を生みます`);
+  }
+}
+
+class FlyableBird extends Bird{
+  constructor(name){
+    super(name);
+  }
+
+  fly(){
+    console.log(`${this.name}が飛びました`);
+  }
+}
+
+const bd1 = new Bird('ペンギン');
+bd1.chirp();
+Bird.explain('カラス');
+
+const bd2 = new FlyableBird('スズメ');
+bd2.fly();
