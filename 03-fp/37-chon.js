9e5089444e320cacb8e41ce9be50281bcaeff665
Hirochon <heacet43@gmail.com>
2020-08-06 10:18:11 +0900

    [mod] 見やすく修正

--- 03-fp/37-chon.js
+++ 03-fp/37-chon.js
@@ -11,8 +11,11 @@
 // const simpleCurriedMulti = n => m => n * m;
 // console.log(simpleCurriedMulti(2)(4));
 
+const primary_multi = (n, m) => n * m;
+console.log(primary_multi(3, 5)); // 15
+
 const multi = n => m => n * m;
-console.log(multi(3)(5));
+console.log(multi(3)(5)); // 15
 
 const triple = multi(3);
-console.log(triple(5));
+console.log(triple(5)); // 15
