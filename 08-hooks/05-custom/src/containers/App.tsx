97906839f9940dfba6de5b0600a41e7d59e0e0d6
Hirochon <heacet43@gmail.com>
2020-08-07 15:55:33 +0900

    [add] Custom-Hooksを扱っていく

--- 08-hooks/05-custom/src/containers/App.tsx
+++ 08-hooks/05-custom/src/containers/App.tsx
@@ -2,6 +2,7 @@ import React, { FC, useEffect, useState } from 'react';
 
 import AppComponent from '../components/App';
 
+// const 関数名 = (引数の変数名: 引数の型): [返り値１の型, 返り値２の関数] => {}
 const useTimer = (limitSec: number): [number, () => void] => {
   const [timeLeft, setTimeLeft] = useState(limitSec);
 
@@ -9,14 +10,15 @@ const useTimer = (limitSec: number): [number, () => void] => {
     setTimeLeft(limitSec);
   };
 
+  // ①コンポーネントのレンダリング直前にtickとtimerIdが動く。
   useEffect(() => {
     const tick = () => {
       setTimeLeft(prevTime => (prevTime === 0 ? limitSec : prevTime - 1));
     };
     const timerId = setInterval(tick, 1000);
 
-    return () => clearInterval(timerId);
-  }, [limitSec]);
+    return () => clearInterval(timerId);  // コンポーネントのアンマウント時にcrearIntervalを実行。時間経過を止める??
+  }, [limitSec]); // 再レンダリング時にlimitSecが変更されていれば、①に戻る。
 
   return [timeLeft, reset];
 };
